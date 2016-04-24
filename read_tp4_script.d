6d3798ed1bfe76c80af9f961b31608c0fa6de6c8
CarlosDiogo01 <carlos.sa01@gmail.com>
2016-04-24 16:42:50 +0100

    Organized files

---
+++ read_tp4_script.d
@@ -0,0 +1,37 @@
+#!/usr/sbin/dtrace -s
+
+#pragma D option quiet
+
+syscall::open*:entry
+/execname=="iozone" & uid==29214/
+{
+	self->path = copyinstr(arg1);
+	self->flag = arg2;
+}
+
+syscall::open*:return
+/self->path=="iozone.tmp" /
+{
+	self->start_clock = timestamp;
+	total=0;
+	flag = 1;
+	size = 0;
+}
+
+syscall::read:return
+/self->start_clock > 0/
+{
+	self->read_size = (arg0/1024);	
+	size = size + self->read_size;
+}
+
+syscall::close*:entry
+/self->path=="iozone.tmp"/
+{
+	self->stop_clock = timestamp;
+	self->elapsed = self->stop_clock - self->start_clock;
+	total = total + self->elapsed; 
+	printf("%-12s %s\n","SIZE","ELAPSED TIME");
+	printf("%-12d %d\n",size,total);
+	flag = 0;
+}
--- write_tp4_script.d
+++ write_tp4_script.d
@@ -11,31 +11,32 @@ syscall::open*:entry
 syscall::open*:return
 /self->path=="iozone.tmp"/
 {
-	total_time=0;
+	flag = 1;
 	size = 0;
-	flag = 1; 
+	total=0;
 }
 
 syscall::write:entry
 /flag == 1/
 {
-	self->start_time = timestamp;
-	self->w_size = arg1;	
-	size = size + self->w_size;
+	self->start_clock = timestamp;
+	self->write_size = (arg2/1024);	
+	size = size + self->write_size;
 }
 
 syscall::write:return
-/self->start_time > 0 & self->w_size > 0/
+/self->start_clock > 0 & self->write_size > 0/
 {
 	self->stop_time = timestamp;
-	self->elapsed = self->stop_time - self->start_time;
-	total_time = total_time + self->elapsed; 
+	self->elapsed = self->stop_time - self->start_clock;
+	total = total + self->elapsed; 
 }
 
 
 syscall::close*:entry
 /self->path=="iozone.tmp"/
 {
-	printf("Size: %d\nElapsed Time: %d\n",size,total_time);
+	printf("%-12s %s\n","TAMANHO","TEMPO TOTAL");
+	printf("%-12d %d\n",size,total);
 	flag = 0;
 }
